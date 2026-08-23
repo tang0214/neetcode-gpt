c469a569388e6ce6c331ed1dee1aa0926109cec7
張語棠 <tangzxcv74@gmail.com>
2026-08-24 03:26:20 +0800

    Update Linear regression training (Math Foundations 6/6)

--- foundations/linear_regression_training.py
+++ foundations/linear_regression_training.py
@@ -5,6 +5,7 @@ from numpy.typing import NDArray
 class Solution:
     def get_derivative(self, model_prediction: NDArray[np.float64], ground_truth: NDArray[np.float64], N: int, X: NDArray[np.float64], desired_weight: int) -> float:
         # note that N is just len(X)
+        #print("X[:, desired_weight]",X[:, desired_weight])
         return -2 * np.dot(ground_truth - model_prediction, X[:, desired_weight]) / N
 
     def get_model_prediction(self, X: NDArray[np.float64], weights: NDArray[np.float64]) -> NDArray[np.float64]:
@@ -31,7 +32,7 @@ class Solution:
             prediction = self.get_model_prediction(X, weights)
 
             for j in range(len(weights)):
-                print("prediction: ", prediction)
+                #print("prediction: ", prediction)
                 grad = self.get_derivative(prediction, Y, n, X, j)
                 #print("grad: ",grad)
                 weights[j] -= self.learning_rate * grad
